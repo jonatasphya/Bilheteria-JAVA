7d10d77d3911081822d1645872eceb00892b1375
jonatasphya <git@github.com>
2022-11-18 21:07:28 -0300

    alterações

--- src/Principal.java
+++ src/Principal.java
@@ -7,39 +7,7 @@ public class Principal {
 		
 		MenuController menu = new MenuController(); 
 		menu.menuFilmes();
-		
-		    
-		    
-		
-			/*Bilheteria bilhete = new Bilheteria();
-	    
 
-	        i = 1;
-	        System.out.println("CINEMA 99\nSESSÕES");
-	        for(String sessao : bilhete.arrSessoes)
-	        {
-	            System.out.println(i++ + " - " + sessao);
-	        }
-	        System.out.print("OPÇÃO: ");
-	        opc = in.nextInt();
-	        bilhete.sessao = bilhete.arrSessoes.get(opc - 1);
-
-	        System.out.println("CINEMA 99\nINGRESSOS");
-	        System.out.println("QTD INGRESSOS (INTEIRA): ");
-	        opc = in.nextInt();
-	        bilhete.qtdInteira = opc;
-
-	        System.out.println("CINEMA 99\nINGRESSOS");
-	        System.out.println("QTD INGRESSOS (MEIA ENTRADA): ");
-	        opc = in.nextInt();
-	        bilhete.qtdMeia = opc;
-	        
-	        String total = NumberFormat.getCurrencyInstance(new Locale("pt-BR", "BR"))
-	                .format(bilhete.calculaIngressos(bilhete.qtdMeia, bilhete.qtdInteira));
-	        
-	        System.out.println("---RECIBO---\n\nINTEIRAS: " + bilhete.qtdInteira + " x R$ 32,00\nMEIAS: " + bilhete.qtdMeia + " x R$ 16,00");
-	        System.out.println("VALOR TOTAL: " + total);
-	        System.out.println("\nVOCÊ IRÁ ASSISTIR: " + bilhete.tituloFilme + " NA SESSÃO DE " + bilhete.sessao);*/
 		
 	}
 
